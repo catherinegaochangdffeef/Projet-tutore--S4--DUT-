338fe254603b848930b4eb89724b4da566b5fd67
Chloé ZANNIER <63238911+chloe025@users.noreply.github.com>
2021-03-01 16:57:45 +0100

    ajout fonctions

--- main.ml
+++ main.ml
@@ -8,3 +8,14 @@ let rec input_valid prompt is_valid cast =
       urs ()
   in
   print_endline prompt; urs ();
+ 
+ ;
+ let string_of_player = function 
+  | H n when (1 <= n && n <= 4)-> "Joueur " ^ (string_of_int n) ^ " (humain)"
+  | B n when (1 <= n && n <= 4)-> "Joueur " ^ (string_of_int n) ^ " (bot)"
+  | _ -> failwith "Le joueur doit etre un nombre en 1 et 4"
+
+;
+let rec string_of_dominoes = function 
+  |[] -> ""
+  |D(x, y)::l -> (string_of_int x)^"-"^(string_of_int y)^" "^( string_of_dominoes l)
